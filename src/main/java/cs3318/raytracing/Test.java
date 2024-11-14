7de98a0b999ce248492dad829e8bd10dbe898b34
BadWolf02 <130060024+BadWolf02@users.noreply.github.com>
2024-11-14 15:10:23 +0000

    Update print in test.java for testing purpose

--- src/main/java/cs3318/raytracing/Test.java
+++ src/main/java/cs3318/raytracing/Test.java
@@ -2,6 +2,6 @@ package cs3318.raytracing;
 
 public class Test {
     public static void main(String [] args){
-        System.out.println("hello");
+        System.out.println("hello world");
     }
 }
