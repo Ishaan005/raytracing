f65d728b654e09e2a10de4794a91936334ea3241
Conor Lane <122453586@umail.ucc.ie>
2024-11-14 15:20:05 +0000

    Conor testing

--- src/main/java/cs3318/raytracing/Test.java
+++ src/main/java/cs3318/raytracing/Test.java
@@ -3,5 +3,7 @@ package cs3318.raytracing;
 public class Test {
     public static void main(String [] args){
         System.out.println("hello");
+        System.out.println("This is conor testing from home");
     }
+
 }
